112882397e6c233222b378749ab5dc22c9782f49
Pedro Laizo <58554976+PedroLaizo@users.noreply.github.com>
2023-06-20 10:01:10 -0300

    Web_server.py

--- Web_server.py
+++ Web_server.py
@@ -1,42 +1,40 @@
 import os
 import sys
-from flask import Flask, request, send_from_directory, render_template
+from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
-# Rota para listar os arquivos e diretórios no diretório raiz
 @app.route('/')
 def list_files():
     root_dir = app.config['ROOT_DIR']
     return generate_directory_listing(root_dir)
 
-# Rota para exibir o conteúdo do arquivo
-@app.route('/file/<path:path>')
-def show_file_content(path):
+@app.route('/<path:path>')
+def download_file(path):
     root_dir = app.config['ROOT_DIR']
     file_path = os.path.join(root_dir, path)
 
-    if os.path.isfile(file_path):  # Verifica se é um arquivo
-        with open(file_path, 'r') as file:
-            content = file.read()
-            return render_template('file_content.html', content=content)
+    if os.path.isfile(file_path):
+        return send_from_directory(root_dir, path, as_attachment=True)
+    elif os.path.isdir(file_path):
+        return generate_directory_listing(file_path)
     else:
         return "Erro: Página não encontrada", 404
 
-# Função para gerar a listagem de arquivos e diretórios em um determinado diretório
-def generate_directory_listing(directory):
-    try:
-        files = os.listdir(directory)
-    except FileNotFoundError:
-        return "Erro: Diretório não encontrado"
+@app.route('/HEADER')
+def show_header():
+    headers = request.headers  # Obtém o cabeçalho da requisição
+    return str(headers)
 
+def generate_directory_listing(directory):
+    files = os.listdir(directory)
     file_list = "<ul>"
     for file in files:
         file_path = os.path.join(directory, file)
-        file_url = os.path.relpath(file_path, app.config['ROOT_DIR']).replace("\\", "/")
+        file_url = file_path.replace(app.config['ROOT_DIR'], '', 1).lstrip('/')
         if os.path.isdir(file_path):
-            file_url += '/' if not file_url.endswith('/') else ''  # Adiciona ou remove a barra final na URL
-        file_list += f"<li><a href='/file/{file_url}'>{file}</a></li>"
+            file += '/'
+        file_list += f"<li><a href='{file_url}'>{file}</a></li>"
     file_list += "</ul>"
     return file_list
 
@@ -52,7 +50,7 @@ if __name__ == "__main__":
             print("Diretório inválido.")
             sys.exit(1)
         app.config['ROOT_DIR'] = os.path.abspath(root_dir)
-        app.env = 'production'  # Define o ambiente como 'production'
+        app.env = 'production'
         app.run(host='0.0.0.0', port=port, debug=False)
     except ValueError:
         print("A porta deve ser um número inteiro válido.")
