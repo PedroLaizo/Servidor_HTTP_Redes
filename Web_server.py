1026bc137a6b8323328c8c1bdfd18fffd3c0f4e6
Pedro Laizo <58554976+PedroLaizo@users.noreply.github.com>
2023-06-20 08:50:47 -0300

    Update Web_server.py

--- Web_server.py
+++ Web_server.py
@@ -4,35 +4,39 @@ from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
+# Rota para listar os arquivos e diretórios no diretório raiz
 @app.route('/')
 def list_files():
     root_dir = app.config['ROOT_DIR']
     return generate_directory_listing(root_dir)
 
+# Rota para realizar o download de um arquivo ou navegar em um diretório
 @app.route('/<path:path>')
 def download_file(path):
     root_dir = app.config['ROOT_DIR']
     file_path = os.path.join(root_dir, path)
 
-    if os.path.isfile(file_path):
+    if os.path.isfile(file_path):  # Verifica se é um arquivo
         return send_from_directory(root_dir, path, as_attachment=True)
-    elif os.path.isdir(file_path):
+    elif os.path.isdir(file_path):  # Verifica se é um diretório
         return generate_directory_listing(file_path)
     else:
         return "Erro: Página não encontrada", 404
 
+# Rota para exibir o cabeçalho da requisição
 @app.route('/HEADER')
 def show_header():
     headers = request.headers  # Obtém o cabeçalho da requisição
     return str(headers)
 
+# Função para gerar a listagem de arquivos e diretórios em um determinado diretório
 def generate_directory_listing(directory):
-    files = os.listdir(directory)
+    files = os.listdir(directory)  # Lista os arquivos e diretórios no diretório
     file_list = "<ul>"
     for file in files:
         file_path = os.path.join(directory, file)
-        file_url = file_path.replace(app.config['ROOT_DIR'], '', 1).lstrip('/')
-        if os.path.isdir(file_path):
+        file_url = file_path.replace(app.config['ROOT_DIR'], '', 1).lstrip('/')  # Gera a URL relativa ao diretório raiz
+        if os.path.isdir(file_path):  # Verifica se é um diretório
             file += '/'
         file_list += f"<li><a href='{file_url}'>{file}</a></li>"
     file_list += "</ul>"
@@ -55,4 +59,3 @@ if __name__ == "__main__":
         print("A porta deve ser um número inteiro válido.")
     except OSError as e:
         print(f"Erro ao iniciar o servidor: {e}")
-
